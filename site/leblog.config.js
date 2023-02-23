7f7fa7efe3551717786a3ed368472fce3bc9d9b8
Nick Goodall <nbgoodall@users.noreply.github.com>
2023-02-23 12:43:27 +0000

    Add changelog to site

---
+++ site/leblog.config.js
@@ -0,0 +1,5 @@
+export default {
+  collections: {
+    changelog: '../package/CHANGELOG.md'
+  }
+}
--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { Entry } from 'leblog'
+  /** @type {import('./$types').PageData} */
+  export let data
+
+  // console.log('hello', data)
+</script>
+
+<h1>leblog</h1>
+
+<ul class="!list-none !pl-0">
+  {#each data.changelog as entry}
+    <li class="!pl-0">
+      <h2 class="text-4xl">
+        <!-- <a href="/{ post.slug }" class="no-underline" data-sveltekit-noscroll>{ post.title }</a> -->
+      </h2>
+
+      <Entry {entry} />
+    </li>
+
+{/each}
+
+</ul>
